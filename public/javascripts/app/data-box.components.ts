1a33c06e1eaf902181738724c312b4b3a0a9cfbc
achieven <achievendar@gmail.com>
2016-11-26 14:04:16 +0200

    added unadded file from commit

---
+++ public/javascripts/app/data-box.components.ts
@@ -0,0 +1,22 @@
+import {Input, Component} from 'angular2/core';
+import {TypesDataComponent} from './types-data.components.js'
+import {SeveritiesDataComponent} from './severities-data.components.js'
+import {SourcesDataComponent} from './sources-data.components.js'
+
+@Component({
+    selector: 'data-box',
+    templateUrl: 'templates/data-box.html',
+    directives: [TypesDataComponent, SeveritiesDataComponent, SourcesDataComponent]
+})
+
+export class DataBoxComponent {
+    header: string = '';
+    data: Object = {};
+
+    @Input() set _data(_data:any) {
+        if (_data) {
+            this.header = _data.header
+            this.data = _data;
+        }
+    }
+}
---
+++ public/javascripts/app/types-data-row.components.ts
@@ -0,0 +1,8 @@
+import {Input, Component} from 'angular2/core';
+
+@Component({
+    selector: 'types-data',
+    templateUrl: 'templates/types-data.html'
+})
+
+export class TypesDataComponent {}
