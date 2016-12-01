bf2a7b2f3e1c51a4bd44e1891e78b8440387ae8d
achieven <achievendar@gmail.com>
2016-12-01 13:04:26 +0200

    fixed bar chart to bar and value together

--- public/javascripts/app/sources-data-row.components.ts
+++ public/javascripts/app/sources-data-row.components.ts
@@ -24,16 +24,17 @@ export class SourcesDataRowComponent {
 
         function renderChart() {
             var chartWidth = $(chartSelector).width(), chartHeight = $(chartSelector).height()
-            var leftOffset = 5
-            var leftBarX1 = leftOffset
-            var leftBarX2 = 5 + (thisComponent.value * (chartWidth - 10) / 100)
+            var roundingBarOffset = 3
+            var leftBarX1 = roundingBarOffset
+            var leftBarX2 = roundingBarOffset + (thisComponent.value * (chartWidth - (roundingBarOffset * 2)) / 100)
             var rightBarX1 = leftBarX2
-            var rightBarX2 = chartWidth - 5
+            var rightBarX2 = chartWidth - roundingBarOffset
             var bothBarsY = chartHeight / 2
+            var brightStroke = '#1D384B', darkStroke = '#8AB4D4'  
 
             var chartSvg = '<svg class="ct-chart-bar ct-horizontal-bars" height="100%" width="100%">' +
-                '<g class="ct-series ct-series-b"> <line class="ct-bar" style="stroke: #1D384B;" x1="' + rightBarX1 + '" x2="' + rightBarX2 + '" y1="' + bothBarsY + '" y2="' + bothBarsY + '"></line>' +
-                '<g class="ct-series ct-series-a"> <line class="ct-bar" style="stroke: #8AB4D4;" x1="' + leftBarX1 + '" x2="' + leftBarX2 + '" y1="' + bothBarsY + '" y2="' + bothBarsY + '"></line>' +
+                '<g class="ct-series ct-series-b"> <line class="ct-bar" style="stroke: ' + brightStroke + '; stroke-width: 5px;" x1="' + rightBarX1 + '" x2="' + rightBarX2 + '" y1="' + bothBarsY + '" y2="' + bothBarsY + '"></line>' +
+                '<g class="ct-series ct-series-a"> <line class="ct-bar" style="stroke: ' + darkStroke + '; stroke-width: 5px" x1="' + leftBarX1 + '" x2="' + leftBarX2 + '" y1="' + bothBarsY + '" y2="' + bothBarsY + '"></line>' +
                 '</svg>'
             $(chartSelector).html(chartSvg)
         }
