7a7c85017ccb2c7578a2cf74719f6c007c90a025
achieven <achievendar@gmail.com>
2016-12-05 13:31:56 +0200

    removed the token thing

--- server/util/util.js
+++ server/util/util.js
@@ -6,23 +6,6 @@ var Util = {
     parseJson: function(){
         var json = fs.readFileSync('./server/assets/data.json', 'utf8')
         return json
-    },
-    storeToken: function(callback){
-        if(!this.tokenList){
-            this.tokenList = []
-        }
-        var self = this
-        crypto.randomBytes(48, function (err, buffer) {
-            var token = buffer.toString('hex');
-            self.tokenList[sha256(token)] = true
-            callback(token)
-        })
-    },
-    getToken: function(cookies){
-        var tokenList = this.tokenList, token = cookies.auth_token
-        if(tokenList && token && tokenList[sha256(token)]){
-            return true
-        }
     }
 }
 
